07ab2ce9d07d9e247239721962b89baccc1793be
Tyler Ruckinger <t.ruckinger@gmail.com>
2020-02-13 08:25:40 -0500

    add winapi, need to sync

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -6,10 +6,24 @@ pub mod session;
 
 
 
-// #[cfg(windows)]
-// #[test]
-// fn windows_base_test() {
-//     use winapi::um::{
-        
-//     };
-// }
+#[cfg(windows)]
+#[test]
+fn windows_base_test() -> Result<(), &'static str> {
+    use winapi::um::{
+        consoleapi::{CreatePseudoConsole, ClosePseudoConsole},
+        namedpipeapi::{CreatePipe},
+        winnt::{HANDLE},
+    };
+
+    // Create pipes
+    let hPtyInput = 0 as HANDLE;
+    let hPtyOutput = 0 as HANDLE;
+    let hProcInput = 0 as HANDLE;
+    let hProcOutput = 0 as HANDLE;
+
+    // proc -> pty
+    // pty -> proc ?
+    
+
+    Ok(())
+}
