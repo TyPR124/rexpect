9e85f8f2a90789a7cce12e959cd28c981c7edab6
TyPR124 <t.ruckinger@gmail.com>
2020-02-13 16:35:37 -0500

    slightly more windows stuff, need to sync

--- src/windows/mod.rs
+++ src/windows/mod.rs
@@ -8,18 +8,56 @@ pub mod session;
 
 #[cfg(windows)]
 #[test]
-fn windows_base_test() -> Result<(), &'static str> {
+fn windows_base_test() -> Result<(), String> {
+    use winapi::shared::winerror::{
+        S_OK
+    };
     use winapi::um::{
         consoleapi::{CreatePseudoConsole, ClosePseudoConsole},
+        errhandlingapi::{GetLastError},
+        handleapi::{CloseHandle},
         namedpipeapi::{CreatePipe},
+        wincontypes::COORD,
         winnt::{HANDLE},
     };
+    struct ClosableHandle(HANDLE);
+    impl Drop for ClosableHandle {
+        fn drop(&mut self) {
+            unsafe { CloseHandle(self.0); }
+        }
+    }
+    impl Default for ClosableHandle {
+        fn default() -> Self {
+            Self(0 as HANDLE)
+        }
+    }
+    impl ClosableHandle {
+        pub fn handle(&self) -> &HANDLE {
+            &self.0
+        }
+        pub fn handle_mut(&mut self) -> &mut HANDLE {
+            &mut self.0
+        }
+    }
 
     // Create pipes
-    let hPtyInput = 0 as HANDLE;
-    let hPtyOutput = 0 as HANDLE;
-    let hProcInput = 0 as HANDLE;
-    let hProcOutput = 0 as HANDLE;
+    let mut hPtyInput = ClosableHandle::default();
+    let mut hPtyOutput = ClosableHandle::default();
+    let mut hProcInput = ClosableHandle::default();
+    let mut hProcOutput = ClosableHandle::default();
+    let mut hPty = ClosableHandle::default();
+    unsafe {
+        if CreatePipe(hPtyInput.handle_mut(), hProcOutput.handle_mut(), 0 as *mut _, 0) == 0 {
+            Err("cannot create proc->pty pipe")?;
+        }
+        if CreatePipe(hProcInput.handle_mut(), hPtyOutput.handle_mut(), 0 as *mut _, 0) == 0 {
+            Err("cannot create pty->proc pipe")?;
+        }
+        let r = CreatePseudoConsole(COORD { X: 100, Y: 100 }, *hPtyInput.handle(), *hPtyOutput.handle(), 0, hPty.handle_mut());
+        if r != S_OK {
+            Err(format!("cannot create pseudoconsole: error {:#X}", r))?
+        }
+    }
 
     // proc -> pty
     // pty -> proc ?
